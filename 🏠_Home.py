19b6c242135cfda79871ec5a517cc2ca535fdcf1
David Lemming <d.lemming@l3bm.com>
2025-06-03 15:04:17 +0200

    Update st pages

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -1,7 +1,10 @@
 import streamlit as st
 
 st.set_page_config(page_title="NYC Taxi Trips", page_icon="🚖", layout="wide")
-
 st.title('NYC Yellow Taxi Trip Data')
 
-# Only 
+
+# Streamlit pages should only contain minimal code
+# to glue different components together.
+# All logic, plotting, and non-streamlit related functions
+# should live in /components or /logic.
