c6a090130b383eff5a0a092feb57358312418c5d
David Lemming <david.lemming28@gmail.com>
2025-06-04 19:06:04 +0200

    Add charts onto grid on dashboard

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -1,12 +1,40 @@
-import plotly.express as px
 import streamlit as st
+
 from logic.data_loader import load_taxi_data
+
 from components.passenger_count import show_passenger_count_bar_chart
+from components.health import render_health_component
+from components.univariate import (
+    render_bar_chart,
+    render_boxplot,
+    render_histogram,
+    render_pie_chart
+)
 
 st.set_page_config(page_title="NYC Taxi Trips", page_icon="🚖", layout="wide")
-st.title('NYC Yellow Taxi Trip Data')
+#st.title('NYC Yellow Taxi Trip Data')
 
-# Daten laden 
 df = load_taxi_data()
 
-show_passenger_count_bar_chart(df)
+# Top row
+col1, col2 = st.columns([1,2])
+with col1:
+    st.markdown("### Data Integrity")
+    render_health_component(df)
+with col2:
+    st.markdown("### Histogram")
+    render_histogram(df)
+
+# Bottom row
+col3, col4, col5 = st.columns(3)
+with col3:
+    st.markdown("### Bar Chart")
+    render_bar_chart(df)
+with col4:
+    st.markdown("### Pie Chart")
+    render_pie_chart(df)
+with col5:
+    st.markdown("### Box Plot")
+    render_boxplot(df)
+
+#show_passenger_count_bar_chart(df)
