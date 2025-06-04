7d8255a0ed2c6daeaeb9a4b73ea7cafb1cb50e5e
David Lemming <david.lemming28@gmail.com>
2025-06-04 19:05:04 +0200

    Optimize univariate plots

--- components/univariate.py
+++ components/univariate.py
@@ -6,25 +6,30 @@ from logic.univariate import get_metrical_columns, get_categorical_columns
 def render_boxplot(df: pd.DataFrame):
     numeric = get_metrical_columns(df)
     if numeric:
-        col = st.selectbox("Boxplot Column", numeric)
+        col = st.selectbox(
+            "Boxplot Column",
+            numeric,
+            label_visibility="collapsed",
+            index=8 # total_amount
+        )
         __boxplot(df, col)
 
 def render_histogram(df: pd.DataFrame):
     numeric = get_metrical_columns(df)
     if numeric:
-        col = st.selectbox("Histogram Column", numeric)
+        col = st.selectbox("Histogram Column", numeric, label_visibility="collapsed")
         __histogram(df, col)
 
 def render_bar_chart(df: pd.DataFrame):
     categorical = get_categorical_columns(df)
     if categorical:
-        col = st.selectbox("Bar Chart Column", categorical)
+        col = st.selectbox("Bar Chart Column", categorical, label_visibility="collapsed", index=3)
         __bar_chart(df, col)
 
 def render_pie_chart(df: pd.DataFrame):
     categorical = get_categorical_columns(df)
     if categorical:
-        col = st.selectbox("Pie Chart Column", categorical)
+        col = st.selectbox("Pie Chart Column", categorical, label_visibility="collapsed", index=0)
         __pie_chart(df, col)
 
 
