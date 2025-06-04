cd47254f311483ca573e8ea96c1404da4149c7a4
David Lemming <david.lemming28@gmail.com>
2025-06-04 17:12:29 +0200

    Add caching for dataloader

--- logic/data_loader.py
+++ logic/data_loader.py
@@ -1,5 +1,7 @@
 import pandas as pd
+import streamlit as st
 
+@st.cache_data
 def load_taxi_data():
     """Lädt die Taxi-Daten aus einer CSV-Datei und gibt die ersten nrows zurück."""
     return pd.read_csv(r"data/data-small.csv")
